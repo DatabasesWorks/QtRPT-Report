c40d814d479f6fde4db88c2449885e8df5bc6cf7
Aleksey Osipov <aliks-os@ukr.net>
2017-04-09 16:21:32 +0300

    logger

--- CommonFiles/XYZ_Logger.cpp
+++ CommonFiles/XYZ_Logger.cpp
@@ -1,3 +1,25 @@
+/*
+Name: XYZ
+Version: 2.0.1
+Programmer: Aleksey Osipov
+E-mail: aliks-os@ukr.net
+Web-site: http://www.aliks-os.tk
+
+Copyright 2012-2017 Aleksey Osipov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include "XYZ_Logger.h"
 
 /*!
@@ -9,13 +31,12 @@
  */
 
 /*!
- \fn XYZLogger::XYZLogger(QObject *parent, QString fileName,
-               QPlainTextEdit *editor) : QObject(parent)
+ \fn XYZLogger::XYZLogger(QObject *parent, QString fileName, QPlainTextEdit *editor) : QObject(parent)
     Constructs a XYZLogger object with the given \a parent.
 */
-XYZLogger::XYZLogger(QObject *parent, QString fileName,
-               QPlainTextEdit *editor) : QObject(parent) {
-
+XYZLogger::XYZLogger(QObject *parent, QString fileName, QPlainTextEdit *editor)
+: QObject(parent)
+{
     m_editor = editor;
     m_showDate = true;
     if (!fileName.isEmpty()) {
@@ -29,16 +50,17 @@ XYZLogger::XYZLogger(QObject *parent, QString fileName,
  \fn XYZLogger::write(const QString &value)
   XYZLogger::write write a \a value to the logger
  */
-void XYZLogger::write(const QString &value) {
+void XYZLogger::write(const QString &value)
+{
     QString text = value;// + "\n";
     if (m_showDate)
         text = QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss ") + text;
     QTextStream out(file);
     out.setCodec("UTF-8");
-    if (file != 0) {
+    if (file != nullptr) {
         out << text;
     }
-    if (m_editor != 0)
+    if (m_editor != nullptr)
         m_editor->appendPlainText(text);
 }
 
@@ -46,12 +68,14 @@ void XYZLogger::write(const QString &value) {
  \fn XYZLogger::setShowDateTime(bool value)
  XYZLogger::setShowDateTime sets \a value show or not date and time during logging
  */
-void XYZLogger::setShowDateTime(bool value) {
+void XYZLogger::setShowDateTime(bool value)
+{
     m_showDate = value;
 }
 
 //! Destructor
-XYZLogger::~XYZLogger() {
-    if (file != 0)
+XYZLogger::~XYZLogger()
+{
+    if (file != nullptr)
         file->close();
 }
--- CommonFiles/XYZ_Logger.h
+++ CommonFiles/XYZ_Logger.h
@@ -1,3 +1,25 @@
+/*
+Name: XYZ
+Version: 2.0.1
+Programmer: Aleksey Osipov
+E-mail: aliks-os@ukr.net
+Web-site: http://www.aliks-os.tk
+
+Copyright 2012-2017 Aleksey Osipov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #ifndef XYZ_LOGGER_H
 #define XYZ_LOGGER_H
 
@@ -11,8 +33,8 @@ class XYZLogger : public QObject
 {
     Q_OBJECT
 public:
-    explicit QxyzLogger(QObject *parent, QString fileName, QPlainTextEdit *editor = 0);
-    ~QxyzLogger();
+    explicit XYZLogger(QObject *parent, QString fileName, QPlainTextEdit *editor = 0);
+    ~XYZLogger();
     void setShowDateTime(bool value);
 
 private:
